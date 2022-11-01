9152e6fe8f93f0da6edd720c1232d40935289ba0
kaziasifjawwad <kaziasifjawwad@gmail.com>
2022-11-01 22:34:03 +0600

    added currency exchange service to microservice

---
+++ spring-boot-microservice/currency_exchange_service/src/test/java/com/example/currency_exchange_service/CurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.currency_exchange_service;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class CurrencyExchangeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
