30cf6c74e24183c5abbafdc44c4ce2b59a596622
kaziasifjawwad <kaziasifjawwad@gmail.com>
2022-11-02 00:16:54 +0600

    configured currency exchange service with config server

---
+++ spring-boot-microservice/currency_exchange_service/src/main/java/com/example/currency_exchange_service/controller/CurrencyExchangeController.java
@@ -0,0 +1,35 @@
+package com.example.currency_exchange_service.controller;
+
+import com.example.currency_exchange_service.pojo.ExchangeValue;
+import lombok.RequiredArgsConstructor;
+import org.springframework.core.env.Environment;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@RestController
+@RequestMapping("/currency-exchange")
+@RequiredArgsConstructor
+public class CurrencyExchangeController {
+
+    private final Environment environment;
+
+    @GetMapping("/from/{from}/to/{to}")
+    public ResponseEntity<ExchangeValue> retriveExchangeValue(@PathVariable String from, @PathVariable String to) {
+        return ResponseEntity.ok(new ExchangeValue()
+                .setFrom("USD")
+                .setTo("BDT")
+                .setConvertion((BigDecimal.valueOf(45L)))
+                .setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty(("local.server.port")
+                                ))
+                        )
+                )
+        );
+    }
+
+}
---
+++ spring-boot-microservice/currency_exchange_service/src/main/java/com/example/currency_exchange_service/pojo/ExchangeValue.java
@@ -0,0 +1,19 @@
+package com.example.currency_exchange_service.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.experimental.Accessors;
+
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@Accessors(chain = true)
+public class ExchangeValue {
+
+    private Long id;
+    private String from;
+    private String to;
+    private BigDecimal convertion;
+    private int port;
+}
