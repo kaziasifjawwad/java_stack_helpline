3b2c4df55cccc681accf3c2931ac373336d55052
kaziasifjawwad <kaziasifjawwad@gmail.com>
2022-12-12 19:55:41 +0600

    added liquibase

---
+++ spring_boot/liquibase/src/main/java/com/jawwad/liquibase/LiquibaseApplication.java
@@ -0,0 +1,13 @@
+package com.jawwad.liquibase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquibaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquibaseApplication.class, args);
+	}
+
+}
