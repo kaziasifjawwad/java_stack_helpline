b5ff79813e95bbf5d90881eb428cab43a6d5ce15
kaziasifjawwad <kaziasifjawwad@gmail.com>
2022-12-13 18:45:17 +0600

    removed unnecessary import

--- spring_boot/liquibase_practice/src/main/java/com/jawwad/liquibase/LiquibaseApplication.java
+++ spring_boot/liquibase_practice/src/main/java/com/jawwad/liquibase/LiquibaseApplication.java
@@ -2,12 +2,11 @@ package com.jawwad.liquibase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 
 @SpringBootApplication
 public class LiquibaseApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(LiquibaseApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(LiquibaseApplication.class, args);
+    }
 }
