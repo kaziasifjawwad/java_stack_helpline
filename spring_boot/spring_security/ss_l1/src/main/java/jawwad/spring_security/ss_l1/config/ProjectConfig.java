7d3bc225512b6c52a2b4cb6cfa3a8cda4b14045f
kaziasifjawwad <kaziasifjawwad@gmail.com>
2022-12-29 08:42:53 +0600

    spring security : created dummy controller class

---
+++ spring_boot/spring_security/ss_l1/src/main/java/jawwad/spring_security/ss_l1/config/ProjectConfig.java
@@ -0,0 +1,8 @@
+package jawwad.spring_security.ss_l1.config;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ProjectConfig {
+
+}
--- spring_boot/spring_security/ss_l1/src/main/java/jawwad/spring_security/ss_l1/controller/TestController.java
+++ spring_boot/spring_security/ss_l1/src/main/java/jawwad/spring_security/ss_l1/controller/TestController.java
@@ -1,4 +1,12 @@
 package jawwad.spring_security.ss_l1.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class TestController {
+    @GetMapping("/test")
+    public String test() {
+        return "test";
+    }
 }
