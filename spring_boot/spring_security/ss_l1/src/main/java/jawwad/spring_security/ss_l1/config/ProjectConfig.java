148566a178dbec3af4700f093c9b339eb3a6369b
kaziasifjawwad <kaziasifjawwad@gmail.com>
2022-12-30 06:48:58 +0600

    spring security : added password encoder

--- spring_boot/spring_security/ss_l1/src/main/java/jawwad/spring_security/ss_l1/config/ProjectConfig.java
+++ spring_boot/spring_security/ss_l1/src/main/java/jawwad/spring_security/ss_l1/config/ProjectConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -17,5 +19,10 @@ public class ProjectConfig {
                 .build();
         usd.createUser(u1);
         return usd;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return NoOpPasswordEncoder.getInstance();
     }
 }
