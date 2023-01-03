56faea56eab7e1d30cf7ca63d1fb7f780fc92605
kaziasifjawwad <kaziasifjawwad@gmail.com>
2023-01-03 06:58:21 +0600

    spring secrurity : UserDetailsService project generated

---
+++ spring_boot/spring_security/ss_l2/src/main/java/com/jawwwad/ss_l2/SsL2Application.java
@@ -0,0 +1,13 @@
+package com.jawwwad.ss_l2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsL2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsL2Application.class, args);
+    }
+
+}
---
+++ spring_boot/spring_security/ss_l2/src/test/java/com/jawwwad/ss_l2/SsL2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jawwwad.ss_l2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SsL2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
